ef92557e76393011e3b8113298c3fe67915370a0
Ricardo Silva <ricardo@salamandra.cc>
2014-10-06 06:20:09 -0300

    fix initialize script

--- 9.3/bin/initialize-database.sh
+++ 9.3/bin/initialize-database.sh
@@ -3,11 +3,9 @@ set -e
 
 chown -R postgres "$PGDATA"
 
-if [ -z "$(ls -A "$PGDATA")" ]; then
-  gosu postgres initdb
 
-  sed -ri "s/^#(listen_addresses\s*=\s*)\S+/\1'*'/" "$PGDATA"/postgresql.conf
+gosu postgres initdb
 
-  { echo; echo 'host all all 0.0.0.0/0 trust'; } >> "$PGDATA"/pg_hba.conf
+sed -ri "s/^#(listen_addresses\s*=\s*)\S+/\1'*'/" "$PGDATA"/postgresql.conf
 
-fi
+{ echo; echo 'host all all 0.0.0.0/0 trust'; } >> "$PGDATA"/pg_hba.conf
