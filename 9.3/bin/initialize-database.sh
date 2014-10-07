c2ce6061ec038af252c4f7feb0418a7fba2b2d13
Ricardo Silva <ricardo@salamandra.cc>
2014-10-07 00:17:07 -0300

    fix restore script

--- 9.3/bin/initialize-database.sh
+++ 9.3/bin/initialize-database.sh
@@ -13,3 +13,4 @@ gosu postgres initdb
 sed -ri "s/^#(listen_addresses\s*=\s*)\S+/\1'*'/" "$PGDATA"/postgresql.conf
 
 { echo; echo 'host all all 0.0.0.0/0 trust'; } >> "$PGDATA"/pg_hba.conf
+:
--- 9.3/bin/restore-database-backup.sh
+++ 9.3/bin/restore-database-backup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-gosu postgres envdir /etc/wal-e.d/env /usr/local/bin/wal-e backup-fetch $PGDATA LATEST
+envdir /etc/wal-e.d/env /usr/local/bin/wal-e backup-fetch $PGDATA LATEST
